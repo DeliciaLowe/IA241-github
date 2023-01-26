d276ed9fbaba70f1bec6e24a562dc293fcf4945c
EC2 Default User <ec2-user@ip-172-31-26-92.ec2.internal>
2023-01-26 21:45:36 +0000

    my lab2.py file

--- lab2.py
+++ lab2.py
@@ -1,5 +1,6 @@
 '''
 lab 2
+numbers, variables, and strings
 '''
 
 #6.1
@@ -12,26 +13,29 @@ print(my_id)
 
 #6.3
 #123 = my_id
+#print(my_id)
 
 #6.4
 my_id = your_id = 123
+print(my_id)
 print(your_id)
 
 #6.5
-my_id_str = 123
+my_id_str = '123'
 print(my_id_str)
 
 #6.6
 #print(my_name + my_id)
 
 #6.7 
-#print(my_name + my_id_str)
+print(my_name + my_id_str)
 
 #6.8
 print(my_name * 3)
 #6.9
-print("hello, world. This is my first python string.".split(.))
+print("hello, world. This is my first python string.".split('.'))
 
 #6.10
-
+#message = 'Tom's id is 123'
+#print(message)
 
