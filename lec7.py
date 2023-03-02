3c5eaf0a4a67e3705ecb19dd979ff4d6f24f715d
EC2 Default User <ec2-user@ip-172-31-26-92.ec2.internal>
2023-03-02 21:32:41 +0000

    adding lab7 and lec7

---
+++ lec7.py
@@ -0,0 +1,75 @@
+'''
+
+lec7
+while loop
+continue, break, and pass
+
+'''
+
+#i = 5
+#while i > 0:
+    #print(i)
+    
+#i = 5
+#while i > 0:
+    #i = i - 1
+   # print(i)
+    #i = i - 1
+    
+    #if i == 3:
+        #break
+    
+#for item in ['a','b']:
+    #print(item)
+    #i = 5
+    #while i > 0:
+        #i = i - 1
+        #print(i)
+        
+        #if i == 3:
+            #break
+        
+#i = 5 
+#while i > 0:
+    #i = i - 1
+    #print(i)
+    #i = i - 1
+    
+    #if i == 3:
+        #continue
+    
+    #print(i)
+    
+    
+#i = 5 
+#while i > 0:
+    #i = i - 1
+    #print(i)
+    #i = i - 1
+    
+    #if i == 3:
+        #pass
+    
+    #print(i)
+
+
+#try:
+    #print(1/0)
+
+#except:
+    #print('error')
+    
+# Question in lecture
+
+i = 5
+while i >= 0:
+    
+    
+    
+    try:
+        print(1/(i-3))
+    except:
+        continue 
+    
+    i = i - 1
+
